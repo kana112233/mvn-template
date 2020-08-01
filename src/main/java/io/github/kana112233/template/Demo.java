e466c44ad86d8cd248551cd48382e2bdc18b0422
kana112233 <makey22112@gmail.com>
2020-08-01 14:56:17 +0800

    first commit

---
+++ src/main/java/io/github/kana112233/template/Demo.java
@@ -0,0 +1,7 @@
+package io.github.kana112233.template;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/test/java/io/github/kana112233/template/DemoTest.java
@@ -0,0 +1,13 @@
+package io.github.kana112233.template;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoTest {
+    @Test
+    public void testSmock() {
+        assertEquals("1", "1");
+    }
+
+}
